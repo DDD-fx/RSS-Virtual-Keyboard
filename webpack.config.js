a214a3011ed8e81de8a9577f730f90034a3af916
DDD-fx <d.khoroshkin@gmail.com>
2022-05-09 10:28:03 +0300

    feat: created file structure and npms

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[hash].js',
+        assetModuleFilename: 'assets/images/[name][ext]',
+        path: path.resolve(__dirname, 'dist'),
+        clean: {
+            keep: /\.git/,
+        },
+    },
+    devServer: {
+        static: './dist',
+    },
+    plugins: [new HtmlWebpackPlugin({
+            title: 'Virtual Keyboard',
+            filename: 'index.html',
+            template: "src/index.html"
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
